f4217517f9f1fedd45a40092b1e5891e64aac20b
joe-oui <joey.biebuyck@gmail.com>
2024-05-12 20:41:23 +0200

    basic structure

--- Main.py
+++ Main.py
@@ -2,5 +2,20 @@ from enum import Enum
 import numpy as np
 from matplotlib import pyplot as plt
 
-class Grid:
-   def __init__(self, size, laadplatformen=1):
+class Grid(object):
+   def __init__(self, item_to_pos_dict, size, laadplatformen=1, nr_of_agents=2):
+      self.agents = [] # init hier x agenten
+      self.items_to_pos_dict = item_to_pos_dict
+
+   def find(self, item_name):
+      return self.items_to_pos_dict(item_name)
+
+class Agent(object):
+   def __init__(self, capacity=2):
+      self.goals = []
+      self.path = None
+      self.available = None
+      self.other_agents = None
+
+   def choose_item(self):
+      self.goals.append()
