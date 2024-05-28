0189fa5d70eebbc1de4c0b5647ffc97f959b0f2c
osnolf <163463050+osnolf@users.noreply.github.com>
2024-05-28 15:56:21 +0200

    strategy 1 added

--- Main.py
+++ Main.py
@@ -200,7 +200,31 @@ class Grid(object):  # het logische grid
             for agent in self.agents:
                 agent.play()
 
+def heuristic(a, b):
+    return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+def neighbours(location):
+    return ((location[0]-1, location[1]), (location[0]+1, location[1]), (location[0], location[1]+1), (location[0], location[1]-1))
 
+def strategy_1(available, chosen_items, other_agent_choices, current_position, product_locations_dictonairy):
+    location_available_items = []
+    distance_to_available_items = []
+    for item in available:
+        print("item: ", item)
+        location = product_locations_dictonairy.get(item)
+        location_available_items.append(location)
+
+    if len(other_agent_choices) == 0: #als er nog geen intentions zijn kiezen we het dichtste item
+        for pos in location_available_items:
+            print("position: ", pos)
+            distance_to_available_items.append(math.dist(pos, current_position))
+            print("return: ", available[(distance_to_available_items.index(min(distance_to_available_items)))])
+        return available[(distance_to_available_items.index(min(distance_to_available_items)))]
+    else:
+        for i in location_available_items:
+            print("this is the location of an available item: ", i)
+            print("this is the other agent choice: ", other_agent_choices[-1])
+            distance_to_available_items.append(math.dist(i, product_locations_dictonairy.get(other_agent_choices[-1])))
+        return available[(distance_to_available_items.index(max(distance_to_available_items)))]
 
 class Agent(object):
     def __init__(self, grid, strategy, capacity=2):  # TODO: zorg ervoor dat elke strategie dezelfde parameters neemt (en definieer ze altijd boven alles)
